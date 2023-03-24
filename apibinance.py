ad2df7a53ca12a11a66ea6a67e4d9530efe4bfff
patrick siotti <59841892+patrick-siotti@users.noreply.github.com>
2023-03-23 23:25:04 -0700

    Update apibinance.py

--- apibinance.py
+++ apibinance.py
@@ -1,2 +1,2 @@
-api_key = 'KFweDRFAEza7eBSn7UQCUoILzZazz0mdSZpizeqWBHkCdRprIzHCQanUg3ishlBd'
-api_secret = 'P9y2BIeKaCiEJV5qxogVBrpVNIwI6g5Ceb5DzOrUy8gz48Hl53KptD5dJZOAnezf'
+api_key = ''
+api_secret = ''
